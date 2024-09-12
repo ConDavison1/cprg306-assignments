7e66323f4ae51e6a079f8b6bdf929889416bb441
ConDavison1 <connor.e.davison@edu.sait.ca>
2024-09-12 13:07:10 -0600

    Week 2 Assignment code
    I created the week-2 page. Then created a StudentInfo component containing my name and the link to my GitHub repo.
    I then rendered the StudentInfo component to be under "Shopping List". Lastly edited the Root page to contain a heading and the link to the week-2 page.

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,11 @@
+import Studentinfo from "./student-info";
 
 
 export default function Page() {
     return(
         <main>
-            
+            <h1>Shopping List</h1>
+            <Studentinfo />
         </main>
     );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function Studentinfo(){
+    return(
+        <div>
+            <h3>Connor Davison</h3>
+            <Link href="https://github.com/ConDavison1/cprg306-assignments">GitHub Repo</Link>
+        </div>
+    );
+}
